4d4da54024f9bb9ee113cff1d55e50f667388f59
Mars <52197560+MinkuruDev@users.noreply.github.com>
2021-10-15 20:49:31 +0700

    dasd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>\nsomething')
