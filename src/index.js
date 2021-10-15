06319af5cfe0e2689d7f53c70f21a5c6c858806e
Mars <52197560+MinkuruDev@users.noreply.github.com>
2021-10-15 21:38:34 +0700

    asdasd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = 6969;
+const handlebars  = require('express-handlebars');
 const morgan = require('morgan');
 
 app.use(morgan('combined'))
 
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>\nsomething')
+  res.render('home');
 })
 
 app.listen(port, () => {
